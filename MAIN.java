c86b8c49c459e5442fbc736f3e715bb25d4ef2a3
Emanuel77777777777777 <christosa214@gmail.com>
2023-06-22 14:06:14 +0300

    new file added

--- MAIN.java
+++ MAIN.java
@@ -1,3 +1,12 @@
+class Amanuel_bekele {
+    void amanuel_Bekele() {
+        String name = "Amanuel Bekele";
+        int id_no = 1227;
+        System.out.println(name);
+        System.out.println(id_no);
+
+    }
+}
 
 public class MAIN {
     public static void main(String args[]) {
