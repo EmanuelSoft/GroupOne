22709f298085329f548d70e3fa2dfe34675d47c2
Emanuel77777777777777 <christosa214@gmail.com>
2023-06-22 14:35:11 +0300

    By amanuel bekele

--- MAIN.java
+++ MAIN.java
@@ -1,15 +1,13 @@
-class Amanuel_bekele {
+public class MAIN {
+    public static void main(String args[]) {
+
+    }
+
     void amanuel_Bekele() {
         String name = "Amanuel Bekele";
         int id_no = 1227;
         System.out.println(name);
         System.out.println(id_no);
-
-    }
-}
-
-public class MAIN {
-    public static void main(String args[]) {
 
     }
 }
