01c0eeb67f014f0750d9578ade881dcd569404b9
Emanuel77777777777777 <christosa214@gmail.com>
2023-06-22 11:47:56 +0300

    file added

--- MAIN.java
+++ MAIN.java
@@ -1,3 +1,4 @@
+
 public class MAIN {
     public static void main(String args[]) {
 
---
+++ aman.java
@@ -0,0 +1,10 @@
+public class aman {
+    public static void main(String args[]) {
+        String name = "Amanuel Bekele";
+        int id_no = 1227;
+
+        System.out.println("Software engineering");
+
+    }
+
+}
